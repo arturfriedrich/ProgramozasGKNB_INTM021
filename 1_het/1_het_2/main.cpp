b69875681b6ccce10ba4da7c4d411f1731c4843a
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-09 13:22:50 +0100

    Added 1_het_2

---
+++ 1_het/1_het_2/main.cpp
@@ -0,0 +1,17 @@
+// 1/1 => Kiíratás
+// Ez egy 1 soros megjegyzés
+/* Ez egy több
+						soros
+								megjegyzés */
+
+#include<iostream>
+
+using namespace std;
+ /* Egy névtérben logikailag összefüggő változókat, függvényeket, típusokat tárolunk.
+     Egy osztály/struktúra egyben a nevével azonos nevű névteret is definiál. */
+
+int main() {
+  cout << "\n Ez az első C++ programunk!" << endl; // új sor + puffer ürítés
+  // std::cout << "\n Ez az első C++ programunk!" << std::endl; // új sor + puffer ürítés
+return 0;
+}
