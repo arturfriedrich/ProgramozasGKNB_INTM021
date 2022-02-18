a87029910bedba30afecd8dd90c6603731b00bcf
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-18 11:55:17 +0100

    Added 1_100.cpp

---
+++ 1_het/feladatok/1_100.cpp
@@ -0,0 +1,15 @@
+// Írjuk ki a számokat 1-től 100-ig
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "A program kiírja 1-től 100-ig az egész számokat." << endl;
+    int i = 1;
+    while (i < 101) {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
