90571a2e5a639fb32fbb59c159354ad8eb75dc5e
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-20 23:33:51 +0100

    Added minmax1.cpp

---
+++ 2_het/elmelet/minmax1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Adjon meg nem negativ egesz szamokat,\n"
+       << "megkeressuk kozottuk a minimalisat es a maximalisat.\n"
+       << "Kilepes negativ szam megadasaval.\n";
+  int db=0, akt=1; // inicializacio
+  int min, max;
+  while(akt >= 0) {
+    cout << "Kovetkezo szam: ";
+    cin >> akt;
+    if(akt >= 0) {
+      if(db == 0) min = max = akt; // tobbszoros hozzarendeles
+      else if(akt > max) max = akt;
+      else if(akt < min) min = akt;
+      db++; // noveles eggyel
+    }
+  }
+  if(db > 0) cout <<   "A minimum: " << min
+                  << "\nA maximum: " << max << '\n';
+  else cout << "Nem adott meg adatokat.\n";
+  return 0;
+}
