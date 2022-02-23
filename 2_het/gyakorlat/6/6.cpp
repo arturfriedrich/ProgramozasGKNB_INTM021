6eafee911011f6d82f2d2c51d7e27a9227e2a688
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-23 11:48:23 +0100

    Added 6

---
+++ 2_het/gyakorlat/6/6.cpp
@@ -0,0 +1,19 @@
+// szöveg végének megadása
+
+#include<iostream>
+#include<cstdio> // Ctrl+D == EOF
+
+using namespace std;
+
+int main() {
+    char kar;
+	int szam=1;
+    while(cout << "\n Kérem a(z) " << szam << ". karaktert: ",
+        kar=cin.get(), kar!=EOF) {
+	    szam++;
+    cout << "A megadott karakter a(z): "  << kar << endl;
+	}
+    cout << "\n Az Ön kilépő karaktere a(z): "  << kar 
+         << " amely a(z) " << szam << "." << endl;
+return 0;
+}
