24cd818c7cfbab38ac32855341b497094396164e
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-25 20:20:01 +0100

    Added 5

--- 2_het/gyakorlat/5/5.cpp
+++ 2_het/gyakorlat/5/5.cpp
@@ -1,17 +1,18 @@
 // vessző operátor a ciklusfejben
 
 #include<iostream>
+
 using namespace std;
 
 int main() {
     char kar;
-    int szam=1;
+	int szam=1;
     while(cout << "\n Kérem a(z) " << szam << ". karaktert: ",
         kar=cin.get(), kar!='\n') {
 	    szam++;
-    cout << "A megadott karakter a(z): "  << kar << endl;
+        cout << "A megadott karakter a(z): "  << kar << endl;
 	}
     cout << "\n Az Ön kilépő karaktere a(z): "  << kar 
          << " amely a(z) " << szam << "." << endl;
-return 0;
+    return 0;
 }
