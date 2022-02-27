57a4fffbfa674e2c96042d9c9ba722f1b7da1ff4
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-02-27 23:39:37 +0100

    Added kettes

---
+++ 3_het/elmelet/kettes/kettes2/kettes2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  char b[100];
+  int d, i;
+  cout << "Adjon meg egy tizes szamrendszerbeli szamot!\n";
+  cin >> d;
+  i = 0;
+  while(d > 0) {
+    b[i] = d%2+'0'; d /= 2; i++;
+  }
+  cout << "Kettes szamrendszerben: ";
+  i--;
+  while(i >= 0) {
+    cout << b[i]; i--;
+  }
+  cout << endl;
+  return 0;
+}
