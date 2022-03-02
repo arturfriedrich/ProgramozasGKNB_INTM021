73da35127f8fe160fbfe46bf820f03d1e8eaa1f9
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-02 23:47:48 +0100

    Added csere

--- 3_het/feladatok/csere/csere.cpp
+++ 3_het/feladatok/csere/csere.cpp
@@ -16,7 +16,38 @@ int main() {
         cin >> szamok[i];
         i++;
     }
-    int cs_1, cs_2;
-    cout << "Adja meg a "
+    int cs_1=0, cs_2=0;
+    do {
+        cout << "Adja meg az első sorszámot ( 1-10 ): ";
+        cin >> cs_1;
+    } while ( 10 < cs_1 || 1 > cs_1 );
+    do {
+        cout << "Adja meg az második sorszámot ( 1-10 ): ";
+        cin >> cs_2;
+    } while ( 10 < cs_2 || 1 > cs_2 );
+    i = 0;
+    int csere_1, csere_2, csere;
+    while ( i < N ) {
+        if ( i == cs_1-1 ) {
+            csere_1 = szamok[i];
+        } else if ( i == cs_2-1 ) {
+            csere_2 = szamok[i];
+        }
+        i++;
+    }
+    i = 0;
+    while ( i < N ) {
+        if ( szamok[i] == csere_1 ) {
+            szamok[i] = csere_2;
+        } else if ( szamok[i] == csere_2 ) {
+            szamok[i] = csere_1;
+        }
+        i++;
+    }
+    i = 0;
+    while ( i < N ) {
+        cout << szamok[i] << endl;
+        i++;
+    }
     return 0;
 }
