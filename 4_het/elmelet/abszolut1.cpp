45e5f540f8d3fdd04bf97d5e39e343c7c2f3e276
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-10 08:53:26 +0100

    Added abszolut

---
+++ 4_het/elmelet/abszolut1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double v, abs;
+  cout << "Szam: "; cin >> v;
+  cout << "Abszolut erteke: ";
+  
+  
+  if(v < 0.) abs = -v;
+  else abs = v;
+  
+  
+  cout << abs;
+  return 0;
+}
---
+++ 4_het/elmelet/abszolut2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double v, abs;
+  cout << "Szam: "; cin >> v;
+  cout << "Abszolut erteke: ";
+  
+  
+  abs = v<0. ? -v : v;
+  
+  
+  
+  cout << abs;
+  return 0; 
+}
