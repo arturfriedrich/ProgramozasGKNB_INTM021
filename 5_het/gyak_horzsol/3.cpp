344bdc4dfc234565ddf238215246ea58dd6f09f5
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-03-23 09:00:22 +0100

    Update 3.cpp

--- 5_het/gyak_horzsol/3.cpp
+++ 5_het/gyak_horzsol/3.cpp
@@ -26,7 +26,7 @@ void kiir(int tomb[], int ig)
     }
 }
 
-void buborek(int tb[], int *lt)
+void buborek(int tb[], int *lt)     // a * paraméternél mutatja, hogy ez cím szerint van megadva
 {
    for(int v=(*lt)-1; v>=1; v--) {
         for(int e=0, csere; e<v; e++) {
@@ -34,14 +34,17 @@ void buborek(int tb[], int *lt)
                 { csere=tb[e]; tb[e]=tb[e+1]; tb[e+1]=csere; }
         }  // belső rendező ciklus
     } // külső rendező ciklus
-	 cout << endl; (*lt)++;
+	 cout << endl; (*lt)+=2;
 }
 
 int main() {
 	int prtln[MAX]={0};
 	int n=beker(prtln);
 	kiir(prtln, n);
-    buborek(prtln, &n);
-	kiir(prtln, n);
+    buborek(prtln, &n);     // a & a címképző operátor, ilyenkor a & utáni változó cím szerint van 
+	kiir(prtln, n);         // megadva, ilyenkor tudja a többi függvény, hogy meg van változtatva másik függvények által
   return 0;
 }
+
+// ha egy likális változó változik és ezt függvények között átadjuk és ezeken belül változtatjuk akkor a címére kell
+// mutatnunk
