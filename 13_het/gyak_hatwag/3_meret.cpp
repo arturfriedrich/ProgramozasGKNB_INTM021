e759a0c92729e4a8807ebed5cbf341894f705b6d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-05-19 15:53:58 +0200

    Create 3_meret.cpp

---
+++ 13_het/gyak_hatwag/3_meret.cpp
@@ -0,0 +1,24 @@
+// Írjon programot, amely meghatározza egy adott fájl méretét! Az eredményt jelenítse meg a képernyőn!
+
+#include <iostream>
+#include <fstream>
+
+#define NEV "3_meret.cpp"
+
+using namespace std;
+
+int main() {
+    ifstream f(NEV);
+    if ( f.is_open() ) {
+        f.seekg(0, ios_base::end);                                      // pozícionálja a mutatót a fájl végére
+        streampos vege = f.tellg();
+        cout << "A fájl mérete " << vege << " bájt." << endl;
+        f.close();
+    } else {
+        cerr << "Fájl megnyitási hiba." << endl;
+    }
+
+    return 0;
+}
+
+// edit: linuxos gépen pontos, windows az fos és ezért ott nem lesz pontos <3
