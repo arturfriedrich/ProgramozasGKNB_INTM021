7b6ea4d4b179fd623469d2a8fc9fe8e7f3f626ee
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-05-26 08:53:24 +0200

    1_feladat added

---
+++ vizsgak/2020_05_21/1_feladat/1_feladat.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string szokozkiszed(string &s) {
+    s.erase(remove(s.begin(), s.end(), ' '), s.end());
+    cout << s;
+
+    return "";
+}
+
+void betubolszam(string &s) {
+    for ( unsigned int i=0; i<s.length(); i++ ) {
+        if ( (int)s[i] - 96 > 0 ) {
+            cout << ((int)s[i] - 96);
+        } else cout << s[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    string teszt1 = "a b c d e f"; cout << teszt1 << "\n";
+    cout << szokozkiszed(teszt1) << "\n";
+    betubolszam(teszt1);
+
+    cout << endl;
+
+    string teszt2 = "abc  XYZ"; cout << teszt2 << "\n";
+    cout << szokozkiszed(teszt2) << "\n";
+    betubolszam(teszt2);
+
+    cout << endl;
+
+    string teszt3 = "ff  ff  aa"; cout << teszt3 << "\n";
+    cout << szokozkiszed(teszt3) << "\n";
+    betubolszam(teszt3);
+
+    cout << endl;
+
+    string teszt4 = "abcdef"; cout << teszt4 << "\n";
+    cout << szokozkiszed(teszt4) << "\n";
+    betubolszam(teszt4);
+
+    return 0;
+}
+
+/*  int kar;
+	int szam=1;
+    while(cout << "\n Kérem a(z) " << szam << ". karaktert: ",
+        kar=cin.get(), kar!=EOF) {
+	    szam++;
+        cout << "A megadott karakter a(z): "  << kar << endl;
+	}
+    cout << "\n Az Ön kilépő karaktere a(z): "  << kar 
+         << " amely a(z) " << szam << "." << endl;
+    return 0; */
